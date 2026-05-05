c74097f973710f8ae5688a9db9ca094e75e4825e
thestackersudo <reset39594765@gmail.com>
2026-05-05 22:16:35 +0100

    Variable Addtions and PrintTable

--- util.lua
+++ util.lua
@@ -1,5 +1,14 @@
 local Utils = {}
 
+Utils.Workspace = game:GetService("Workspace")
+Utils.ReplicatedStorage = game:GetService("ReplicatedStorage")
+Utils.DataRemote = replicatedStorage
+    :WaitForChild("BridgeNet")
+    :WaitForChild("dataRemoteEvent")
+Utils.Players = game:GetService("Players")
+Utils.VirtualUser = game:GetService("VirtualUser")
+
+
 function Utils.BuildArgs(pathTable, nValue)
 	local data = {}
 
@@ -18,7 +27,11 @@ function Utils.RemoveSpaces(text)
 	return text:gsub(" ", "")
 end
 
-
+function Utils.PrintTable(table)
+	for i,v in pairs(table) do
+		print(i,v)
+	end
+end
 
 
 return Utils
